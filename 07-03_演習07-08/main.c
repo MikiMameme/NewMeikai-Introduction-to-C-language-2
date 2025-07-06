dcf2e53690e25b0e55ea9fee4178ca45516e4f3d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-06 17:07:32 +0900

    07-03_演習07-08

---
+++ 07-03_演習07-08/main.c
@@ -0,0 +1,12 @@
+/* •‚“®¬”“_Œ^‚Ì‘å‚«‚³‚ğ•\¦‚·‚é */
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("sizeof(float)       = %u\n", (unsigned)sizeof(float));
+	printf("sizeof(double)      = %u\n", (unsigned)sizeof(double));
+	printf("sizeof(long double) = %u\n", (unsigned)sizeof(long double));
+	
+	return 0;
+}
