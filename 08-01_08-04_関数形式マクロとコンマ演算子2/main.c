c6d57602d27070945218dbae277f809ea5fe8c9d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-07 19:53:39 +0900

    08-01_08-04_関数形式マクロとコンマ演算子2
    コンマ演算子で結び付けて実行可能にした。

---
+++ 08-01_08-04_関数形式マクロとコンマ演算子2/main.c
@@ -0,0 +1,20 @@
+/* 警報を発しながら表示を行うマクロ（コンマ演算子） */
+
+#include<stdio.h>
+
+#define puts_alert(str) ( putchar('\a'), puts(str) )
+
+int main(void)
+{
+	int n;
+	
+	printf("整数を入力してください:");
+	scanf("%d", &n);
+	
+	if(n)
+		puts_alert("その数はゼロではありません。");
+	else
+		puts_alert("その数はゼロです。");
+		
+		return 0;
+}
