add3879ae8ac3df7c9db4ea318e34eb40a75487b
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-10 17:51:09 +0900

    08-05_演習08-09

---
+++ 08-05_演習08-09/main.c
@@ -0,0 +1,17 @@
+/* 標準入力に現れた改行の数を表示する */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int ch;
+	int n_count = 0;
+	
+	while((ch = getchar()) != EOF)
+		if(ch == '\n')
+			n_count++;
+			
+	printf("行数:%d\n", n_count);
+	
+	return 0;
+}
