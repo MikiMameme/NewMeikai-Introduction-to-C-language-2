1f2151c9fc1b6ac662b820df7b61fed1b936791f
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-11 20:01:59 +0900

    09-01_09-05_文字列を初期化して表示

---
+++ 09-01_09-05_文字列を初期化して表示/main.c
@@ -0,0 +1,16 @@
+/* •¶š—ñ"12345"‚ğ‘®‰»‚µ‚Ä•\¦ */
+
+#include<stdio.h>
+
+int main(void)
+{
+	char str[] = "12345";
+	
+	printf("%s\n",   str);
+	printf("%3s\n",  str);
+	printf("%.3s\n", str);
+	printf("%8s\n",  str);
+	printf("%-8s\n", str);
+	
+	return 0;
+}
