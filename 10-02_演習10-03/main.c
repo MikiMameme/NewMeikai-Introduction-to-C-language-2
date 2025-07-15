c0429b5ab4a5b96e7969908b950ba89033e3f7cd
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-15 12:58:41 +0900

    10-02_演習10-03
    三つの整数を昇順に並べる

---
+++ 10-02_演習10-03/main.c
@@ -0,0 +1,38 @@
+/* 三つの整数を昇順に並べる */
+
+#include<stdio.h>
+
+/* pxとpyが指すオブジェクトの値を交換 */
+void swap(int *px, int *py)
+{
+	int temp = *px;
+	*px = *py;
+	*py = temp;
+}
+
+/* *n1≦n2≦n3となるように並べる */
+void sort2(int *n1, int *n2, int *n3)
+{
+	if(*n1 > *n2)	swap(n1, n2);
+	if(*n2 > *n3)	swap(n2, n3);
+	if(*n1 > *n2)	swap(n1, n2);
+}
+
+int main(void)
+{
+	int na, nb, nc;
+	
+	puts("二つの整数を入力してください。");
+	printf("整数A:");	scanf("%d", &na);
+	printf("整数B:");	scanf("%d", &nb);
+	printf("整数C:");	scanf("%d", &nc);
+	
+	sort2(&na, &nb, &nc);
+	
+	puts("昇順にしました。");
+	printf("整数Aは%dです。\n", na);
+	printf("整数Bは%dです。\n", nb);
+	printf("整数Cは%dです。\n", nc);
+	
+	return 0;
+}
