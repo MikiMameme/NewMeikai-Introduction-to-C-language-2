71c10b3c72006129301e9f55187b7e98af5a005e
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-15 13:32:22 +0900

    10-03_10-09_ポインタの配列
    配列のアドレス（要素のポインタ）を表示

---
+++ 10-03_10-09_ポインタの配列/main.c
@@ -0,0 +1,15 @@
+/* 配列のアドレス（要素のポインタ）を表示 */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i;
+	int a[5] = {1, 2, 3, 4, 5};
+	int *p = a;
+	
+	for(i = 0; i < 5; i++)
+		printf("&a[%d] = %p p+%d = %p\n", i, &a[i], i, p + i);
+		
+		return 0;
+}
