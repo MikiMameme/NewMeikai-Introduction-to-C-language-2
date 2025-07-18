f55fea3ba5d6fe4076d48e03cc8d3ad70da351ee
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-18 20:40:06 +0900

    11-01_11-03_配列による文字列とポインタによる文字列の違い2
    ポインタによる文字列の書き換え

---
+++ 11-01_11-03_配列による文字列とポインタによる文字列の違い2/main.c
@@ -0,0 +1,13 @@
+/* ƒ|ƒCƒ“ƒ^‚É‚æ‚é•¶š—ñ‚Ì‘‚«Š·‚¦ */
+
+#include<stdio.h>
+
+int main(void)
+{
+	char *p = "123";
+	printf("p = \"%s\"\n", p);
+	p = "456";
+	printf("p = \"%s\"\n", p);
+	
+	return 0;
+}
