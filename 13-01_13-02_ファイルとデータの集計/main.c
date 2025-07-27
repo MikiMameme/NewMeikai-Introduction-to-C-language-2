d7f4d57a9be854448504bc88d17d6816de34285e
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-27 14:50:45 +0900

    13-01_13-02_ファイルとデータの集計
    身長と体重を読み込んで平均値を求めて表示する

---
+++ 13-01_13-02_ファイルとデータの集計/main.c
@@ -0,0 +1,28 @@
+/* 身長と体重を読み込んで平均値を求めて表示する */
+
+#include<stdio.h>
+
+int main(void)
+{
+	FILE *fp;
+	int ninzu = 0;
+	char name[100];
+	double height, weight;
+	double hsum = 0.0;
+	double wsum = 0.0;
+	
+	if((fp = fopen("test.dat", "r")) == NULL)
+		printf("\aフアイルを開けません。\n");
+	else {
+		while (fscanf(fp, "%s%lf%lf", name, &height, &weight) == 3) {
+			printf("%-10s %5.1f %5.1f\n", name, height, weight);
+			ninzu++;
+			hsum += height;
+			wsum += weight;
+		}
+		printf("----------------------\n");
+		printf("平均        %5.1f %5.1f\n", hsum / ninzu, wsum / ninzu);
+		fclose(fp);
+	}
+	return 0;
+}
