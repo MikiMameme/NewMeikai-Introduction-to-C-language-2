1a96473847823315e3e63c768516d8b787385634
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-27 16:43:54 +0900

    13-01_13-03_日付と時刻の書き込み
    プログラムを実行した日付・時刻をファイルに書き出す

---
+++ 13-01_13-03_日付と時刻の書き込み/main.c
@@ -0,0 +1,25 @@
+/* プログラムを実行した日付・時刻をファイルに書き出す */
+
+#include<stdio.h>
+#include<time.h>
+
+int main(void)
+{
+	FILE *fp;
+	time_t current = time(NULL);
+	struct tm *timer = localtime(&current);
+	
+	if((fp = fopen("dt_dat", "w")) == NULL)
+		printf("\aフアイルを開けません\n");
+	
+	else {
+		
+		printf("現在の日付・時刻を書き出しました。\n");
+		fprintf(fp, "%d %d %d %d %d %d\n",
+			timer->tm_year + 1900, timer->tm_mon + 1, timer->tm_mday,
+			timer->tm_hour,        timer->tm_min,     timer->tm_sec );
+			fclose(fp);
+	}
+	
+	return 0;
+}
