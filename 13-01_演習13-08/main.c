6d745f3ff17528f9417d7e2ad54375c4ed90981f
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-28 19:49:06 +0900

    13-01_演習13-08
    ファイルをコピーする

---
+++ 13-01_演習13-08/main.c
@@ -0,0 +1,32 @@
+/* ファイルをコピーする */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int ch;
+	FILE *sfp;
+	FILE *dfp;
+	char sname[FILENAME_MAX];
+	char dname[FILENAME_MAX];
+	
+	printf("コピ-元フアイル名:");	scanf("%s", sname);
+	printf("コピ-先フアイル名:");	scanf("%s", dname);
+	
+	if((sfp = fopen(sname, "r")) == NULL)
+		printf("\aコピ-元フアイルを開けませんでした。\n");
+	else {
+		if((dfp = fopen(dname, "w")) == NULL)
+			printf("\aコピ-先フアイルを開けませんでした。\n");
+		else {
+			while ((ch = fgetc(sfp)) != EOF) {
+				putchar(ch);
+				fputc(ch, dfp);
+			}
+			fclose(dfp);
+		}
+		fclose(dfp);
+	}
+	
+	return 0;
+}
