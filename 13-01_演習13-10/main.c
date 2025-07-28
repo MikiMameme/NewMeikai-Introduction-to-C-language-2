c4c2d2ceca0115a41a0d59c05a9a24990bedfaeb
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-28 20:01:48 +0900

    13-01_演習13-10
    ファイルの中身を大文字を小文字に変換しながらコピーする

---
+++ 13-01_演習13-10/main.c
@@ -0,0 +1,31 @@
+/* ファイルの中身を大文字を小文字に変換しながらコピーする */
+
+#include<stdio.h>
+#include<ctype.h>
+
+int main(void)
+{
+	int ch;
+	FILE *sfp;
+	FILE *dfp;
+	char sname[FILENAME_MAX];
+	char dname[FILENAME_MAX];
+	
+	printf("コピ-元フアイル名:");	scanf("%s", sname);
+	printf("コピ-先フアイル名:");	scanf("%s", dname);
+	
+	if((sfp = fopen(sname, "r")) == NULL)
+		printf("\aコピ-元フアイルを開けませんでした。\n");
+	else {
+		if((dfp = fopen(dname, "w")) == NULL)
+			printf("\aコピ-先フアイルを開けませんでした。\n");
+		else {
+			while ((ch = fgetc(sfp)) != EOF)
+				fputc(tolower(ch), dfp);
+			fclose(dfp);
+		}
+		fclose(dfp);
+	}
+	
+	return 0;
+}
